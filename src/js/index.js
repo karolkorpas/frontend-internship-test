1e7a2a2977d8f365744a1ee8b1dfa9cf189f4691
Your Name <karol.korpas3@gmail.com>
2019-01-24 12:13:15 +0100

    Task 2

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,25 @@
 /* Here goes your JS code */
+
+           
+function showpopub()
+{
+    this.style.display = "none";
+    pop.style.display = "block";
+}
+function disappearpopub()
+{
+    showpopipform.style.display = "block";
+    pop.style.display = "none";
+}
+
+
+
+var showpopipform = document.getElementById("show-popup-form");
+var pop = document.getElementById("pop");
+var exit = document.getElementById("exit");
+
+showpopipform.onclick = showpopub;
+exit.onclick = disappearpopub;
+
+           
+           
