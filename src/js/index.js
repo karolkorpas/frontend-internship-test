b825c5f626cddcb82d13357f47672e34c106b3a2
Your Name <karol.korpas3@gmail.com>
2019-01-24 15:04:49 +0100

    task3

--- src/js/index.js
+++ src/js/index.js
@@ -17,9 +17,74 @@ function disappearpopub()
 var showpopipform = document.getElementById("show-popup-form");
 var pop = document.getElementById("pop");
 var exit = document.getElementById("exit");
+var thanks = document.getElementById("thanks");
+
 
 showpopipform.onclick = showpopub;
 exit.onclick = disappearpopub;
 
-           
-           
+////check password
+
+var confirm = document.getElementById("form").confirm;
+          
+confirm.onclick = function(e)
+{
+    e.preventDefault();
+    
+    //document.getElementById("form").submit();
+    
+    
+    var email = document.getElementById("form").email;
+    var conditionMail = /(?=.*[@])(?=.*[.])/;
+    
+    var password = document.getElementById("password"); 
+    var conditionPassword = /.{1,}/;
+    
+    var checkbox_1 = document.getElementById("checkbox_1");
+    
+    
+    //mail
+   if(conditionMail.test(email.value))
+       confirm_mail = true;
+   else
+   {
+       confirm_mail = false;
+       email.placeholder = "Invalid format e-mail";
+   }
+   
+   //password
+   if(conditionPassword.test(password.value))
+       confirm_password = true;
+   else
+   {
+       confirm_password = false;
+       password.placeholder = "Enter the password";
+   }
+   
+   //checkbox
+   if(checkbox_1.checked)
+       confirm_checkbox = true;
+   else
+       confirm_checkbox = false;
+    
+   //submit
+    
+    if(confirm_mail === true && confirm_password === true && confirm_checkbox === true)
+    {
+            setTimeout(function()
+            {
+                showpopipform.style.display = "none";
+                pop.style.display = "none";
+                thanks.style.display = "block";
+                
+            }, 3000);
+             setTimeout(function()
+            {
+                document.getElementById("form").submit();
+            }, 6000);
+            
+    }
+    
+    
+    
+};
